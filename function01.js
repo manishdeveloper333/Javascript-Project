3c44db0aff4a0c8fb0e086014c497d07be7dc72f
Manish srivas <76111668+manishdeveloper333@users.noreply.github.com>
2022-04-05 09:34:41 +0530

    Add files via upload

---
+++ function01.js
@@ -0,0 +1,92 @@
+// function is a block of code designed to perform a particular task
+var ducat=function(){return"hello is this funcion"}
+console.log(ducat);
+//alert(typeof ducat);
+//alert(ducat);
+
+let x = 8685;
+console.log(x);
+
+
+
+///CONTROL VALUE
+// IF ELSE FUNCCTION
+let vowel = 1;
+if (vowel == 1)
+{
+    console.log("a");
+}
+else if (vowel == 2)
+{
+    console.log("e");
+}
+else if (vowel == 3)
+{
+    console.log("i");
+}
+else if (vowel == 4)
+{
+    console.log("o");
+}
+
+// logic operator
+
+// vowel consonant
+
+let ch = "a";
+if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+{
+    console.log("vowel");
+}
+else {
+    console.log("consonant")
+}
+
+
+//even number
+
+let even = 1;
+if (even == 2 || even == 4 || even == 6 || even == 8 || even == 10)
+{
+    console.log("even");
+}
+else 
+{
+    console.log("old");
+}
+
+//old number
+let old = 2;
+if ( old == 1 ||  old == 3 || old== 5 ||  old == 7 || old == 9)
+{
+    console.log("old");
+}
+else 
+{
+    console.log("even");
+}
+
+///grade 
+
+
+let per = 90;
+if (per < 60)
+{
+    console.log("weak")
+}
+else if ( per < 60)
+{
+    console.log("good")
+}
+else if ( per < 90)
+{
+    console.log(" very good")
+}
+    else if (per <100)
+{
+    console.log("excellent")
+}
+
+else {
+    console.log("fail");
+}
